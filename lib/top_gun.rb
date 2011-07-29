a032352c1d103e66f299bfa0d6cde1cfd615d3cc
Ben Burkert <ben@benburkert.com>
2011-07-29 00:54:03 -0700

    One of life's simple joys / Is playing with the boys

--- lib/top_gun.rb
+++ lib/top_gun.rb
@@ -35,19 +35,22 @@ module TopGun
     say :text => something_awesome(:mav), :voice  => :bruce
   end
 
+  def self.playing_with_the_boys
+    say :file => 'playing_with_the_boys.txt'
+  end
+
   def self.something_awesome(by_who)
     case by_who
     when :mav
       ["eyell hit the breaks and he will fly right bye",
        "Too close for miss els, eyem switching to guns.",
        "Tower, this is Ghost Rider requesting a flybye",
-       "You don’t have time to think up there. If you think, you’re dead."]
+       "You don't have time to think up there. If you think, you're dead."]
     when :goose
       ["Cum on Mav, do some of that pilot shit!",
        "No, mav, this is not a good idea",
        "You must have carnal knowledge of a lady this time, on the premises",
        "Yee ha!! Jesters dead!"]
     end.sort_by{ rand }.first
-
   end
 end
